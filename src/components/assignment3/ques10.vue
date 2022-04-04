be9f7d4a3d6523c3364af2c733b67c041e782809
srikanth-500appsgit config --global user.email srikanth.munikot@500apps.com <srikanth.munikoti@500apps.com>
2022-04-04 18:25:34 +0530

    test2 commit

---
+++ src/components/assignment3/ques10.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<button @click="getfullname()">Click</button>
+</div>
+</template>
+<script>
+export default{
+name:'computeD',
+data(){
+return{
+firstname:'',
+lastname:''
+}
+},
+methods:{
+getfullname(){
+let fullname = this.firstname+"_"+this.lastname
+document.write("full_name : "+fullname);
+}
+}
+}
+</script>
+
+
+
+
---
+++ src/components/assignment3/ques2.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<form id="demo">
+<p>{{msg.length}}</p>
+<label> QUESTION2 </label>
+<input type="text" name="message" v-model="msg">
+</form>
+</div>
+</template>
+<script>
+export default{
+name:"QuestioN2",
+data(){
+return{
+msg:" "
+}
+},
+}
+</script>
+
+
+
