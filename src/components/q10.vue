6299def7b721e594ae15d4080b482f28aeeed2d8
srikanth-500apps <102018256+srikanth-500apps@users.noreply.github.com>
2022-04-07 11:11:53 +0530

    Add files via upload

---
+++ src/components/q10.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Question1</h1>
+        <button @click="myfun()">click here</button>
+    </div>
+</template>
+<script>
+export default{
+    name:"QuestioN5",
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+    myfun() {
+        try{
+          this.count = this.count * 5;
+          console.log("printing count",this.count);            
+            }
+        catch(ReferenceError){
+            console.log(ReferenceError);
+        }}
+}
+}
+</script>
