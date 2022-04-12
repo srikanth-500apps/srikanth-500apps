c1ef07e3c9d6bc1e729e68dc69d0415c2c5dc56c
srikanth-500apps <102018256+srikanth-500apps@users.noreply.github.com>
2022-04-12 19:04:08 +0530

    Add files via upload

---
+++ src/components/assignment3/ques8.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>Question 8</h1>
+    <p @click="myfunc()"> Click to display the text</P>
+    <textarea id="ta" placeholder="Type here to edit the text"> </textarea>
+</div>
+</template>
+<script>
+export default{
+name:"QuestioN8",
+data(){
+return{
+a:"",
+}
+},
+methods:{
+myfunc(){
+this.a = ''+document.getElementById("ta").value
+document.write(this.a)
+}
+}
+}
+</script>
