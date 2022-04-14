af98ed57a79031cf684e6f10b821f9088161f65b
srikanth-500appsgit config --global user.email srikanth.munikot@500apps.com <srikanth.munikoti@500apps.com>
2022-04-14 18:35:12 +0530

    14-04-2022

---
+++ src/components/sample questions/s3.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>HI</h1>
+        <button type="submit" @click="fun">Fun</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"QuE3",
+    data(){
+        return{
+            a:[1,1,2,2,2,3,4,3,2],
+        }
+    },
+    methods:{
+        fun(){
+       this.a = [...new Set(this.a)]
+       document.write(this.a)
+    }
+    }
+}
+
+
+</script>
